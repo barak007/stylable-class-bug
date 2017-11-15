1099867f92db5c54eaf264a6e717b122d1b72c32
barak igal <baraki@wix.com>
2017-11-15 16:21:37 +0200

    Fix item className
    Hi, 
    The "style" function dose not transform the className for you.
    You need to use the mapping on the style E.g.
    ```jsx
    className={style.item}
    ```

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -4,7 +4,7 @@ import style from './app.st.css';
 export const App: React.SFC<{}> = () => {
   return (
     <div {...style('')}>
-      <div {...style('item')}>Hello World</div>
+      <div {...style(style.item)}>Hello World</div>
     </div>
   );
 };
